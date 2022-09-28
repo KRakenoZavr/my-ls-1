a487884ada9b4cba62ed5ba8ee2dd254548d19a3
Arman Kuzembay <kuzembay@gmail.com>
2022-09-28 15:04:42 +0600

    feat: bold text dirs and links

--- utils/colors.go
+++ utils/colors.go
@@ -2,7 +2,7 @@ package utils
 
 var (
 	Reset = "\033[0m"
-	Dir   = "\033[34m" // blue
-	Link  = "\033[36m" // cyan
+	Dir   = "\033[1m\033[34m" // blue
+	Link  = "\033[1m\033[36m" // cyan
 	White = "\033[97m"
 )
